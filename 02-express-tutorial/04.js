f21c840c54b4eeda35d900318fd22474ebdb3201
Paublo Martínez <pamartinez@vdkit.net>
2023-01-04 20:32:45 -0600

    Versionamiento

--- 02-express-tutorial/04.js
+++ 02-express-tutorial/04.js
@@ -1,12 +1,14 @@
 const { application, json } = require('express');
 const express = require('express');
-const app = express();
 const morgan = require('morgan')
+const app = express();
 const { products } = require('./data')
 const logger = require('./logger')
 const authorize = require('./authorize')
 
 //req => imddleware => res
+
+//app.use([logger, authorize])
 app.use(morgan('tiny'))
 
 app.get('/'  , (req, res) => {    
@@ -16,8 +18,6 @@ app.get('/'  , (req, res) => {
 app.get('/about', (req, res) => {    
     res.send('About')
 })
-
-
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000...')
